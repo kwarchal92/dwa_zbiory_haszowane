149bbf493c83a7f0c01becab75625d0ee09e033a
kwarchal <kamilwarchal295@gmail.com>
2024-03-16 18:55:35 +0100

    druga czesc programu

--- src/dwa_zbiory_haszowane/Main.java
+++ src/dwa_zbiory_haszowane/Main.java
@@ -21,6 +21,23 @@ public class Main {
         System.out.println("Liczba elementow w zbiorze 1: " + zbior1.size() + ".");
 
         //utworzenie zbioru zbior2
+        java.util.Set<String> zbior2 = new java.util.HashSet<>();
 
+        //dodawanie 3 nowych imion do zbioru 2
+        zbior2.add("Maria");
+        zbior2.add("Patrycja");
+        zbior2.add("Marzena");
+
+        System.out.println("\nzbior 2: " + zbior2);
+        System.out.println("Liczba elementow w zbiorze 2: " + zbior2.size());
+
+        System.out.println("Czy w zbiorze 2 znajduje się Dorota? " + zbior2.contains("Dorota"));
+
+        zbior1.addAll(zbior2);
+        System.out.println("Po dodaniu zbioru 2 do zbioru 1 zbior 1 zawiera: " + zbior1 + ".");
+        zbior1.removeAll(zbior2);
+        System.out.println("Po usunieciu zbioru 2 do zbioru 1 zbior 1 zawiera: " + zbior1 + ".");
+        zbior1.retainAll(zbior2);
+        System.out.println("Po zachowaniu wspolnych elementow zbiorow zbior 1 i zbior 2, zbior 1 zawiera: " + zbior1 + ".");
     }
 }
